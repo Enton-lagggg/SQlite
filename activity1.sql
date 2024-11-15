813e51835e5b0450c6468681164cc7011ac58134
Enton-lagggg <playstoreowner635@gmail.com>
2024-11-15 16:23:35 +0300

    receive the aggregate function

---
+++ activity1.sql
@@ -0,0 +1,10 @@
+create table person (
+    Surnamne varchar(255),
+    Firstname varchar(255),
+    id int,
+    );
+
+insert into person(Surname, Firstname, id),
+values('Korir', 'Melvin' , 6083892);
+
+drop table person;
